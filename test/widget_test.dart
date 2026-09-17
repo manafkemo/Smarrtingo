5a0077cbffb93e10b98db417db17fcfb92d4da95
Manaf Kemo <manafkemo@gmail.com>
2026-09-17 23:54:46 +0300

    Widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:smart_todo_app/main.dart';
+import 'package:Smarttingo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
